2280e5cdc580116dfdcf089e5dc695b72edcbce3
cedieme <48179656+cedieme@users.noreply.github.com>
2019-03-03 11:31:06 -0500

    Add files via upload

--- profile/name.php
+++ profile/name.php
@@ -3,23 +3,57 @@
 <head>
 <meta charset="ISO-8859-1">
 <title>Maru inc</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <body>
+
+<div class="w3-top">
+  <div class="w3-bar w3-border w3-light-grey">
+    <a href="homepage.html" class="w3-bar-item w3-button">Home</a>
+    <a href="#" class="w3-bar-item w3-button">Link 1</a>
+    <a href="#" class="w3-bar-item w3-button">Link 2</a>
+    <a href="home.html" class="w3-bar-item w3-button w3-black w3-right">login</a>
+  </div>
+</div>
+
+</head>
+
+<br><br>
+<body>
+<p> Please only put text in fields you intend to change</p>
 <tr>
-	<td>Current Name: name</td>
+	<td>Current Name: name</td><br>
 </tr>
 <tr>
-	<td>New Name<br>
+	<td>change Username<br>
     <INPUT style='width:99%' TYPE='Text' Name=penname maxlength=30  size=35 required class='penname' title="Valid characters: A-Z a-z 0-9 . ' -" >
     </td>
 </tr>
 <tr>
-	<td>Current Password<br>
+	<td>change password<br>
+    <INPUT style='width:99%' TYPE='Text' Name=penname maxlength=30  size=35 required class='penname' title="Valid characters: A-Z a-z 0-9 . ' -" >
+    </td>
+</tr>
+<tr>
+	<td>change email<br>
+    <INPUT style='width:99%' TYPE='Text' Name=penname maxlength=30  size=35 required class='penname' title="Valid characters: A-Z a-z 0-9 . ' -" >
+    </td>
+</tr>
+<tr>
+	<td>change address<br>
+    <INPUT style='width:99%' TYPE='Text' Name=penname maxlength=30  size=35 required class='penname' title="Valid characters: A-Z a-z 0-9 . ' -" placeholder="Current Password">
+		</td>
+</tr>
+
+
+<tr>
+	<td>save changes<br>
         <input style='width:99%' type='password' name=oldpassword id=oldpassword required title='Enter your current password'>
         </td>
 </tr>
 <tr>
 	<td>
-    <button type='submit' class='btn btn-block'>Update Username</button>
+    <button type='submit' class='btn btn-block'>Update information</button>
     </td>
 </tr>
 </body>
