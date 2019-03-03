f14089310e1230d34fc25d035be8af8cbcdc84a5
cedieme <48179656+cedieme@users.noreply.github.com>
2019-03-03 10:16:58 -0500

    Add files via upload

---
+++ name.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Maru inc</title>
+<body>
+<tr>
+	<td>Current Name: name</td>
+</tr>
+<tr>
+	<td>New Name<br>
+    <INPUT style='width:99%' TYPE='Text' Name=penname maxlength=30  size=35 required class='penname' title="Valid characters: A-Z a-z 0-9 . ' -" >
+    </td>
+</tr>
+<tr>
+	<td>Current Password<br>
+        <input style='width:99%' type='password' name=oldpassword id=oldpassword required title='Enter your current password'>
+        </td>
+</tr>
+<tr>
+	<td>
+    <button type='submit' class='btn btn-block'>Update Username</button>
+    </td>
+</tr>
+</body>
+</html>
